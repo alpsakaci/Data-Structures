c5b5a61ff2c9af85e85fae293d751a9fc5d4152a
Alp <erenalpsakaci@gmail.com>
2020-09-21 12:34:37 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "list.h"
+#include <string.h>
+
+int main(int argc, char const *argv[]) {
+    // TODO: example usage of data structures
+
+    return 0;
+}
